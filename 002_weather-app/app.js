e0bb3e8c60de02d1cb769384de26a8927f066f47
philgrayphilgray <thephilgray@gmail.com>
2018-06-01 15:51:36 -0400

    Log the formatted_address, lat, and lng. Refactor.

--- 002_weather-app/app.js
+++ 002_weather-app/app.js
@@ -1,14 +1,21 @@
 const request = require('request');
 require('dotenv').config();
 
+const addressQuery = '1600%20pennsylvania%20ave%20nw';
+
 request(
   {
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1600%20pennsylvania%20ave%20nw&key=${
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressQuery}&key=${
       process.env.GEOCODE_API_KEY
     }`,
     json: true
   },
   (error, response, body) => {
-    console.log(body);
+    // console.log(JSON.stringify(body, undefined, 2));
+    const results = body.results[0];
+    const { formatted_address } = results;
+    const { lat, lng } = results.geometry.location;
+    console.log(`Address: ${formatted_address}`);
+    console.log(`Lat: ${lat}, Lng: ${lng}`);
   }
 );
