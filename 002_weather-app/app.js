04d7f934c378b3342dd0a09357ccdd8cc2581970
philgrayphilgray <thephilgray@gmail.com>
2018-06-01 16:57:36 -0400

    Create dark sky api request.

--- 002_weather-app/app.js
+++ 002_weather-app/app.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const yargs = require('yargs');
 require('dotenv').config();
 
@@ -17,10 +18,31 @@ const argv = yargs
 
 const { address } = argv;
 
+const currentWeather = (lat, lng) => {
+  request(
+    {
+      uri: `https://api.darksky.net/forecast/${
+        process.env.DARK_SKY_API_KEY
+      }/${lat},${lng}`,
+      json: true
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const { temperature } = body.currently;
+        console.log(`Temperature: ${temperature}`);
+      } else {
+        console.log('Unable to fetch weather.');
+      }
+    }
+  );
+};
+
 geocode.gecodeAddress(address, (errorMessage, results) => {
   if (errorMessage) {
     console.error(errorMessage);
   } else {
     console.log(JSON.stringify(results, undefined, 2));
+    console.log(`The current weather:`);
+    currentWeather(results.lat, results.lng);
   }
 });
