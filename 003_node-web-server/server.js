dee53a04c3dae93072a441418d3cea69c77590e5
philgrayphilgray <thephilgray@gmail.com>
2018-06-02 10:59:24 -0400

    Complete middleware exercise.

--- 003_node-web-server/server.js
+++ 003_node-web-server/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const hbs = require('hbs');
 
@@ -6,8 +7,24 @@ const port = '3000';
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 
 hbs.registerHelper('screamIt', text => text.toUpperCase());
