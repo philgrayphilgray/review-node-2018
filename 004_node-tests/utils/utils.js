d2c135ca7a39c94676d3e5d97589279b75263223
thephilgray <thephilgray@gmail.com>
2018-06-06 14:58:08 -0400

    Complete async test exercises.

--- 004_node-tests/utils/utils.js
+++ 004_node-tests/utils/utils.js
@@ -1,3 +1,33 @@
 module.exports.add = (a, b) => a + b;
 
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+  }, 0);
+};
+
 module.exports.sqaure = x => x * x;
+
+module.exports.asyncSquare = (x, callback) => {
+  setTimeout(() => {
+    callback(x * x);
+  }, 0);
+};
+
+module.exports.setName = (user, fullName) => {
+  const names = fullName.split(' ');
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+};
+
+module.exports.asyncSetName = (user, fullName) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const names = fullName.split(' ');
+      user.firstName = names[0];
+      user.lastName = names[1];
+      resolve(user);
+    }, 0);
+  });
+};
--- 004_node-tests/utils/utils.test.js
+++ 004_node-tests/utils/utils.test.js
@@ -1,19 +1,70 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   const actual = utils.add(33, 11);
   const expected = 44;
 
-  if (actual !== expected) {
-    throw new Error(`Expected ${expected}, but got  ${actual}`);
-  }
+  // if (actual !== expected) {
+  //   throw new Error(`Expected ${expected}, but got  ${actual}`);
+  // }
+  expect(actual)
+    .toBe(expected)
+    .toBeA('number');
+});
+
+it('should async add two numbers', done => {
+  const expected = 7;
+  utils.asyncAdd(4, 3, sum => {
+    expect(sum).toBe(expected);
+    done();
+  });
 });
 
 it('should square a number', () => {
   const actual = utils.sqaure(9);
   const expected = 81;
 
-  if (actual !== expected) {
-    throw new Error(`Expected ${expected} but got ${actual}`);
-  }
+  // if (actual !== expected) {
+  //   throw new Error(`Expected ${expected} but got ${actual}`);
+  // }
+  expect(actual)
+    .toBe(expected)
+    .toBeA('number');
+});
+
+it('should async square a numbers', done => {
+  const expected = 64;
+  utils.asyncSquare(8, squared => {
+    expect(squared).toBe(expected);
+    done();
+  });
+});
+
+it('should verify first and last names are the same', () => {
+  const user = {};
+  const fullName = 'Phil Gray';
+  const actual = utils.setName(user, fullName);
+
+  const expected = {
+    firstName: 'Phil',
+    lastName: 'Gray'
+  };
+
+  expect(actual.firstName).toBeA('string');
+  expect(actual.lastName).toBeA('string');
+  expect(actual.firstName.length).toBeGreaterThan(0);
+  expect(actual.lastName.length).toBeGreaterThan(0);
+  expect(actual).toInclude(expected);
+});
+
+it('should async set first and last name', async () => {
+  const user = {};
+  const fullName = 'Phil Gray';
+  const expected = {
+    firstName: 'Phil',
+    lastName: 'Gray'
+  };
+  const actual = await utils.asyncSetName(user, fullName);
+  expect(actual).toEqual(expected);
 });
