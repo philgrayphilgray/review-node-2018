e31d527e3356bc17f59636ad84881cdb1037afef
thephilgray <thephilgray@gmail.com>
2018-06-17 21:15:35 -0400

    Add DELETE /users/me/token route.

--- 005_node-todo-api/server/models/User.js
+++ 005_node-todo-api/server/models/User.js
@@ -52,6 +52,15 @@ UserSchema.methods.generateAuthToken = function() {
   return user.save().then(() => token);
 };
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this;
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   const User = this;
   let decoded;
