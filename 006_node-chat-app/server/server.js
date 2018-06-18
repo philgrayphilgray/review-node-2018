f76e30398ce13828d3f17e448ff59aa2d9f0777a
thephilgray <thephilgray@gmail.com>
2018-06-18 16:02:45 -0400

    Add geolocation support via geolocation api.

--- 006_node-chat-app/server/server.js
+++ 006_node-chat-app/server/server.js
@@ -3,7 +3,10 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const { generateMessage } = require('./utils/message.js');
+const {
+  generateMessage,
+  generateLocationMessage
+} = require('./utils/message.js');
 
 const publicPath = path.join(__dirname, '../public/');
 const PORT = process.env.PORT || 3000;
@@ -32,10 +35,13 @@ io.on('connection', socket => {
     io.emit('newMessage', generateMessage(message.from, message.text));
 
     callback('This is from the server.');
-    // socket.broadcast.emit('newMessage', {
-    //   ...newMessage,
-    //   createdAt: new Date().getTime()
-    // });
+  });
+
+  socket.on('createLocationMessage', coords => {
+    io.emit(
+      'newLocationMessage',
+      generateLocationMessage('Admin', coords.latitude, coords.longitude)
+    );
   });
 
   socket.on('disconnect', () => {
--- 006_node-chat-app/server/utils/message.test.js
+++ 006_node-chat-app/server/utils/message.test.js
@@ -2,11 +2,12 @@ const expect = require('expect');
 const Chance = require('chance');
 const { pick } = require('lodash');
 
-const { generateMessage } = require('./message.js');
+const { generateMessage, generateLocationMessage } = require('./message.js');
 
 const chance = new Chance();
 const user = chance.name();
 const text = chance.sentence();
+const coords = chance.coordinates();
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -16,6 +17,27 @@ describe('generateMessage', () => {
     };
     const actual = generateMessage(input.from, input.text);
     expect(pick(actual, ['from', 'text'])).toMatchObject(input);
+    expect(actual.createdAt).toBeTruthy();
+  });
+});
+
+describe('Name of the group', () => {
+  it('should generate correct location object', () => {
+    const from = user;
+    const [latitude, longitude] = coords.split(',').map(x => x.trim());
+    const input = {
+      latitude,
+      longitude
+    };
+    const url = `https://www.google.com/maps?q=${input.latitude},${
+      input.longitude
+    }`;
+    const actual = generateLocationMessage(
+      from,
+      input.latitude,
+      input.longitude
+    );
+    expect(pick(actual, ['from', 'url'])).toMatchObject({ from, url });
     expect(actual.createdAt).toBeTruthy();
   });
 });
