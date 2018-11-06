11dd3afd1d12b4533a180d4c2989dd915f74baca
thephilgray <thephilgray@gmail.com>
2018-11-05 21:20:21 -0500

    Setup project, create routes, views, and serve static css file

---
+++ 014-express-ex2/app.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./routes/');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.listen(3000);
