9dacfaf3c136e634c0e72bffff7f61efabd87329
thephilgray <thephilgray@gmail.com>
2018-12-16 17:20:04 -0500

    complete net example

--- 010_net/net.js
+++ 010_net/net.js
@@ -1,10 +1,36 @@
 process.stdout.write('\u0018[2J\u0018[0;0f');
-
+let counter = 0;
 const server = require('net').createServer();
+let sockets = {};
+
+function timestamp() {
+  const now = new Date();
+  return `${now.getHours()}:${now.getMinutes()}`;
+}
 
 server.on('connection', socket => {
-  console.log('Client connected');
-  socket.write('Welcome new client!\n');
+  socket.id = counter++;
+  console.log(`Client ${socket.id} connected.`);
+  socket.write('Please type your name: ');
+
+  socket.on('data', data => {
+    if (!sockets[socket.id]) {
+      socket.name = data.toString().trim();
+      socket.write(`Welcome ${socket.name}!\n`);
+      sockets[socket.id] = socket;
+      return;
+    }
+    Object.entries(sockets).forEach(([key, cs]) => {
+      if (socket.id == key) return;
+      cs.write(`${socket.name} (${timestamp()}): `);
+      cs.write(data);
+    });
+  });
+
+  socket.on('end', () => {
+    delete sockets[socket.id];
+    console.log(`Client ${socket.id} disconnected.`);
+  });
 });
 
 server.listen(8000, () => console.log('Server bound'));
