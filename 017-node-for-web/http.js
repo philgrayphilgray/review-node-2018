fb9422b2fd380d768b955773d4cc56fa13139315
thephilgray <thephilgray@gmail.com>
2018-12-16 18:33:50 -0500

    include comments with constructor names

--- 017-node-for-web/http.js
+++ 017-node-for-web/http.js
@@ -1,7 +1,10 @@
+// server: http.Server
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
+  // req: http.IncomingMessage
   res.writeHead(200, { 'content-type': 'text/plain' });
+  // res: http.ServerResponse
   res.write('Hello world\n');
 
   setTimeout(function() {
